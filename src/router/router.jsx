c385150f5cb38948a3ac2d6552a234167e03fffa
AKToran <aktoran60@gmail.com>
2025-06-10 00:10:47 +0600

    navbar, footer and register form

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,23 @@
 import { createBrowserRouter } from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Home from '../pages/Home/Home';
+import Register from "../pages/Register/Register";
 
 
 const router = createBrowserRouter([
   {
     path:'/',
-    element: <p>hlleo</p>
+    Component: RootLayout,
+    children:[
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: 'register',
+        Component: Register
+      }
+    ]
   },
 ]);
 
