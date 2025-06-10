ebdd5d59686805a155c0a1e229e5259f580d4602
AKToran <aktoran60@gmail.com>
2025-06-10 22:24:52 +0600

    firebase config

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,25 +1,37 @@
+import Lottie from "lottie-react";
 import React from "react";
+
+const handleRegister =(e)=>{
+  e.preventDefault();
+  const form = e.target;
+  const email = form.email.value;
+  const password = form.password.value;
+
+  console.log(email, password);
+
+
+}
 
 const Register = () => {
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
-          
+          {/* unable to get lottie files  */}
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <fieldset className="fieldset">
+            <h1 className="text-3xl font-bold">Register now!</h1>
+            <form onSubmit={handleRegister} className="fieldset">
               <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input type="email" name="email" className="input" placeholder="Email" />
               <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
+              <input type="password" name="password" className="input" placeholder="Password" />
               <div>
-                <a className="link link-hover">Forgot password?</a>
+                <a className="link link-hover">Already have an account?</a>
               </div>
-              <button className="btn btn-neutral mt-4">Login</button>
-            </fieldset>
+              <button className="btn btn-neutral mt-4">Register</button>
+            </form>
           </div>
         </div>
       </div>
